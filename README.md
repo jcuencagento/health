1fd6e1549c8a3a93294255c99d62abbf396e279a
Javi Cuenca <jcuencagento@gmail.com>
2025-05-26 00:43:49 +0200

    Try starting services

--- README.md
+++ README.md
@@ -3,5 +3,15 @@ My Health website. Interactive and multiuser.
 
  ⚒️ Stack: Node.JS, Express, MySQL, Cloudflare...
 
+
+
+> [!NOTE]
+> Para correr en dominio
+> npm run build
+> sudo cp -r build/* /var/www/healthweb/
+> pm2 start npm --name healthweb-backend -- run start
+> pm2 save && pm2 startup
+
+
 > [!NOTE]
 > Made by Javier Cuenca Gento.
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ChakraProvider><App /> </ChakraProvider>);
+root.render(
+    <BrowserRouter basename="/">
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </BrowserRouter>
+);
